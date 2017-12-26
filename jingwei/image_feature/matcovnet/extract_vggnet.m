52c09bd33fc8e9ac8d9e680f0e387d2937269da9
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2017-12-26 22:34:36 +1100

    x

--- jingwei/image_feature/matcovnet/extract_vggnet.m
+++ jingwei/image_feature/matcovnet/extract_vggnet.m
@@ -1,7 +1,10 @@
 clear
 
-data_folder = '/fishtank/urix/survey/';   % corresponds to SURVEY_DATA
-ds = 'train10k';                          % dataset to be processed
+data_folder = '/home/xiaojie/Projects/data/yfcc100m/';
+ds = 'yfcc8k';
+
+%data_folder = '/fishtank/urix/survey/';   % corresponds to SURVEY_DATA
+%ds = 'train10k';                          % dataset to be processed
 %ds = 'train1m';
 %ds = 'nuswide';
 %ds = 'train10k';
@@ -13,7 +16,7 @@ relu = 1;                                 % Do relu after fc7
 % Job splitting
 this_part = 1;                            % Part to be processed. One could get this from command line or environment.
 parts = 1;                                % Total parts.
-gpuDevice(this_part);                     % Use this GPU id
+%%%gpuDevice(this_part);                     % Use this GPU id
 
 cnn_nets_folder = 'cnn_models/';
 data_output_folder = 'FeatureData';
@@ -67,7 +70,7 @@ fc7 = zeros(descrSize,length(images_paths),'single');
 %% --------------------LOAD MODEL-------------------
 run matconvnet-1.0-beta8/matlab/vl_setupnn
 net = load(preTrainedModel);
-net=vl_simplenn_move(net,'gpu');
+%%%net=vl_simplenn_move(net,'gpu');
 
 %% --------------- NUMBER OF CORES IN THE MACHINE-----------------
 numCores=12;
@@ -137,7 +140,7 @@ for j=1:numTurns
         
         tic;            
         % run the CNN
-        batch = gpuArray(batch);
+        %%%batch = gpuArray(batch);
         res = vl_simplenn(net, batch) ;
         feat_fc7 = squeeze(gather(res(layer_selected).x));
 
