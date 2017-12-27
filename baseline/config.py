8cb9aa6b237640dc307e7c306af6a3ecb2ce8ec9
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2017-12-27 14:34:45 +1100

    x

--- baseline/config.py
+++ baseline/config.py
@@ -4,18 +4,17 @@ import sys
 from os import path
 
 home_dir = path.expanduser('~')
-root_dir = path.join(home_dir, 'Projects/kdgan')
-data_dir = path.join(root_dir, 'data')
+proj_dir = path.join(home_dir, 'Projects')
+root_dir = path.join(proj_dir, 'kdgan')
+data_dir = path.join(proj_dir, 'data')
 logs_dir = path.join(root_dir, 'logs')
 
-
-name = 'train10k'
 survey_code = path.join(root_dir, 'jingwei')
-survey_data = path.join(data_dir, name)
-survey_db = path.join(data_dir, name)
+survey_data = path.join(data_dir, 'jingwei')
+survey_db = path.join(logs_dir, 'jingwei')
 matlab_path = '/usr/local/bin'
-if not path.isfile(path.join(matlab_path, 'matlab')):
-    matlab_path = '/Applications/MATLAB_R2017b.app/bin'
+# if not path.isfile(path.join(matlab_path, 'matlab')):
+#     matlab_path = '/Applications/MATLAB_R2017b.app/bin'
 
 sys.path.insert(0, survey_code)
 
