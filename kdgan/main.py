4b337cbb160ea590e0ac8531981d7e9883cf99bf
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-01-03 22:05:49 +1100

    x

--- kdgan/main.py
+++ kdgan/main.py
@@ -54,6 +54,34 @@ def check_tfrecord(bt_list, batch_size):
                 #     input()
                 print(user_np.shape, image_np.shape, text_np.shape, label_np.shape, image_file_np.shape)
 
+def evaluate(logits, labels, cutoff, normalize):
+    predictions = np.argsort(-logits, axis=1)[:,:cutoff]
+    batch_size, _ = labels.shape
+    scores = []
+    for batch in range(batch_size):
+        label_bt = labels[batch,:]
+        label_bt = np.nonzero(label_bt)[0]
+        prediction_bt = predictions[batch,:]
+        num_label = len(label_bt)
+        present = 0
+        for label in label_bt:
+            if label in prediction_bt:
+                present += 1
+        score = present
+        if score > 0:
+            score *= (1.0 / normalize(cutoff, num_label))
+        # print('score={0:.4f}'.format(score))
+        scores.append(score)
+    score = np.mean(scores)
+    return score
+
+def compute_hit(logits, labels, cutoff):
+    def normalize(cutoff, num_label):
+        return min(cutoff, num_label)
+    hit = evaluate(logits, labels, cutoff, normalize)
+    # print('hit={0:.4f}'.format(hit))
+    return hit
+
 def main(_):
     print('#label={}'.format(config.num_label))
     gen_t = GEN(flags, is_training=True)
