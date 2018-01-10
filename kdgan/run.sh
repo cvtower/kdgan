d4e32db0736ec60a16696e6ae72b7cf16ead527c
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-01-10 22:42:30 +1100

    x

--- kdgan/run.sh
+++ kdgan/run.sh
@@ -2,6 +2,15 @@ kdgan_dir=$HOME/Projects/kdgan/kdgan
 checkpoint_dir=$kdgan_dir/checkpoints
 pretrained_dir=$checkpoint_dir/pretrained
 
+python pretrain_gen.py \
+  --dataset=yfcc10k \
+  --feature_size=4096 \
+  --gen_model_ckpt=$checkpoint_dir/gen_vgg_16.ckpt \
+  --model_name=vgg_16 \
+  --num_epoch=200
+exit
+# 361s best hit=0.6521
+
 python pretrain_dis.py \
   --dataset=yfcc10k \
   --dis_model_ckpt=$checkpoint_dir/dis_vgg_16.ckpt \
@@ -9,6 +18,7 @@ python pretrain_dis.py \
   --model_name=vgg_16 \
   --num_epoch=200
 # 469s best hit=0.6574
+exit
 
 python train_gan.py \
   --dis_model_ckpt=$checkpoint_dir/dis_vgg_16.ckpt \
@@ -21,14 +31,6 @@ python train_gan.py \
   --num_dis_epoch=50 \
   --num_gen_epoch=10
 exit
-
-python pretrain_gen.py \
-  --gen_model_ckpt=$checkpoint_dir/gen_vgg_16.ckpt \
-  --model_name=vgg_16 \
-  --feature_size=4096 \
-  --num_epoch=200
-exit
-# 361s best hit=0.6521
 
 python train_kd.py \
   --gen_model_ckpt=$checkpoint_dir/gen_vgg_16.ckpt \
