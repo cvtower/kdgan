d6db11792d4d88b050fb78c4ef0dcba02f016214
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-01-13 00:21:24 +1100

    x

--- kdgan/run.sh
+++ kdgan/run.sh
@@ -11,8 +11,8 @@ python train_gan.py \
   --dis_weight_decay=0.0 \
   --gen_weight_decay=0.0 \
   --num_epoch=100 \
-  --num_dis_epoch=50 \
-  --num_gen_epoch=50
+  --num_dis_epoch=100 \
+  --num_gen_epoch=10
 exit
 
 python pretrain_dis.py \
