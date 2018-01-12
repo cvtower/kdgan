5eb196982e689b8fc44e54695015a8089560cc24
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-01-12 17:17:18 +1100

    x gi

---
+++ kdgan/config.py
@@ -0,0 +1,42 @@
+import sys
+from os import path
+
+home_dir = path.expanduser('~')
+proj_dir = path.join(home_dir, 'Projects')
+data_dir = path.join(proj_dir, 'data')
+yfcc_dir = path.join(data_dir, 'yfcc100m')
+root_dir = path.join(proj_dir, 'kdgan')
+surv_dir = path.join(yfcc_dir, 'survey_data')
+kdgan_dir = path.join(root_dir, 'kdgan')
+logs_dir = path.join(kdgan_dir, 'logs')
+temp_dir = path.join(kdgan_dir, 'temp')
+ckpt_dir = path.join(kdgan_dir, 'checkpoints')
+image_dir = path.join(yfcc_dir, 'images')
+
+slim_dir = path.join(root_dir, 'slim')
+sys.path.insert(0, slim_dir)
+
+rawtag_file = path.join(yfcc_dir, 'sample_00')
+sample_file = path.join(yfcc_dir, 'sample_09')
+tfrecord_tmpl = '{0}_{1}_{2:03d}.{3}.tfrecord'
+
+user_key = 'user'
+image_key = 'image'
+text_key = 'text'
+label_key = 'label'
+file_key = 'file'
+unk_token = 'unk'
+pad_token = ' '
+
+channels = 3
+num_label = 100
+num_threads = 4
+
+train_batch_size = 32
+valid_batch_size = 100
+
+
+
+
+
+
