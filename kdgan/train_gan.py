c72b8334c4dde0eae2cf7db186b7abdf18fa128b
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-01-13 00:24:46 +1100

    x

--- kdgan/train_gan.py
+++ kdgan/train_gan.py
@@ -27,7 +27,7 @@ tf.app.flags.DEFINE_integer('num_epoch', 20, '')
 tf.app.flags.DEFINE_float('learning_rate', 0.01, '')
 tf.app.flags.DEFINE_float('learning_rate_decay_factor', 0.95, '')
 tf.app.flags.DEFINE_float('min_learning_rate', 0.00001, '')
-tf.app.flags.DEFINE_float('num_epochs_per_decay', 10.0, '')
+tf.app.flags.DEFINE_float('num_epochs_per_decay', 20.0, '')
 tf.app.flags.DEFINE_string('learning_rate_decay_type', 'exponential', 'fixed|polynomial')
 # dis model
 tf.app.flags.DEFINE_float('dis_weight_decay', 0.0, 'l2 coefficient')
