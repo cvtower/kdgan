1baaa5211ab1c017744542dd5d4d450b67e10cf3
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-01-26 18:17:25 +1100

    x

--- kdgan/mdlcompr/pretrain_tch.py
+++ kdgan/mdlcompr/pretrain_tch.py
@@ -32,10 +32,10 @@ tf.app.flags.DEFINE_integer('batch_size', 128, '')
 tf.app.flags.DEFINE_integer('num_epoch', 200, '')
 tf.app.flags.DEFINE_string('optimizer', 'rmsprop', 'adam|sgd')
 # learning rate
-tf.app.flags.DEFINE_float('learning_rate', 0.01, '')
-tf.app.flags.DEFINE_float('learning_rate_decay_factor', 0.94, '')
+tf.app.flags.DEFINE_float('tch_learning_rate', 0.01, '')
+tf.app.flags.DEFINE_float('tch_learning_rate_decay_factor', 0.94, '')
 tf.app.flags.DEFINE_float('end_learning_rate', 0.0001, '')
-tf.app.flags.DEFINE_float('num_epochs_per_decay', 2.0, '')
+tf.app.flags.DEFINE_float('tch_num_epochs_per_decay', 2.0, '')
 tf.app.flags.DEFINE_string('learning_rate_decay_type', 'exponential', 'fixed|polynomial')
 flags = tf.app.flags.FLAGS
 
--- kdgan/mdlcompr/run.sh
+++ kdgan/mdlcompr/run.sh
@@ -2,6 +2,26 @@ kdgan_dir=$HOME/Projects/kdgan/kdgan_xw
 checkpoint_dir=$kdgan_dir/checkpoints
 
 
+for tch_keep_prob in 0.95 0.90 0.85 0.80 0.75
+do
+  for tch_weight_decay in 0.0001 0.00005 0.00001
+  do
+    python pretrain_tch.py \
+      --tch_checkpoint_dir=$checkpoint_dir/mdlcompr_mnist_tch \
+      --tch_save_path=$checkpoint_dir/mdlcompr_mnist_tch/model \
+      --dataset_dir=$HOME/Projects/data/mnist \
+      --tch_keep_prob=$tch_keep_prob \
+      --tch_weight_decay=$tch_weight_decay \
+      --tch_learning_rate=0.001 \
+      --tch_learning_rate_decay_factor=0.96 \
+      --num_epoch=200
+    # target=0.9932
+    # bstacc=0.9951
+    exit
+  done
+done
+exit
+
 
 python pretrain_gen.py \
   --gen_checkpoint_dir=$checkpoint_dir/mdlcompr_mnist_gen \
@@ -38,24 +58,6 @@ python pretrain_dis.py \
 # target=0.9854
 # bstacc=0.9862 # no dropout no l2
 # bstacc=0.9884 # wt dropout wt l2
-exit
-
-
-for tch_keep_prob in 0.95 0.90 0.85 0.80 0.75
-do
-  for tch_weight_decay in 0.0001 0.00005 0.00001
-  do
-    python pretrain_tch.py \
-      --tch_checkpoint_dir=$checkpoint_dir/mdlcompr_mnist_tch \
-      --tch_save_path=$checkpoint_dir/mdlcompr_mnist_tch/model \
-      --dataset_dir=$HOME/Projects/data/mnist \
-      --tch_keep_prob=$tch_keep_prob \
-      --tch_weight_decay=$tch_weight_decay \
-      --num_epoch=200
-    # target=0.9932
-    # bstacc=0.9951
-  done
-done
 exit
 
 
