a6ed01cb03bda15ebd9142a4d444d2e12205be0d
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-01-27 10:47:55 +1100

    x

---
+++ kdgan/mdlcompr_kd/trials/lenet/dis_model.py
@@ -0,0 +1,104 @@
+from kdgan import config
+from kdgan import utils
+
+import tensorflow as tf
+from nets import nets_factory
+from tensorflow.contrib import slim
+
+class DIS():
+  def __init__(self, flags, dataset, is_training=True):
+    self.is_training = is_training
+    
+    num_feature = flags.image_size * flags.image_size * flags.channels
+    # None = batch_size
+    self.image_ph = tf.placeholder(tf.float32, shape=(None, num_feature))
+    self.hard_label_ph = tf.placeholder(tf.float32, shape=(None, flags.num_label))
+
+    # None = batch_size * sample_size
+    self.sample_ph = tf.placeholder(tf.int32, shape=(None, 2))
+    self.dis_label_ph = tf.placeholder(tf.float32, shape=(None,))
+
+    hidden_size = 800
+    self.dis_scope = dis_scope = 'dis'
+    with tf.variable_scope(dis_scope):
+      self.logits = utils.build_mlp_logits(flags, self.image_ph,
+        hidden_size=hidden_size,
+        keep_prob=flags.dis_keep_prob,
+        weight_decay=flags.dis_weight_decay,
+        is_training=is_training)
+      
+      sample_logits = tf.gather_nd(self.logits, self.sample_ph)
+      reward_logits = self.logits
+      # reward_logits = 2 * (tf.sigmoid(reward_logits) - 0.5)
+      reward_logits = tf.sigmoid(reward_logits)
+      self.rewards = tf.gather_nd(reward_logits, self.sample_ph)
+
+      if not is_training:
+        self.predictions = tf.argmax(self.logits, axis=1)
+        return
+
+      save_dict, var_list = {}, []
+      for variable in tf.trainable_variables():
+        if not variable.name.startswith(dis_scope):
+          continue
+        print('%-50s added to DIS saver' % variable.name)
+        save_dict[variable.name] = variable
+        var_list.append(variable)
+      self.saver = tf.train.Saver(save_dict)
+
+      self.global_step = tf.Variable(0, trainable=False)
+      self.learning_rate = utils.get_lr(flags,
+          self.global_step,
+          dataset.num_examples,
+          flags.dis_learning_rate,
+          flags.dis_learning_rate_decay_factor,
+          flags.dis_num_epochs_per_decay,
+          dis_scope)
+      # self.learning_rate = tf.Variable(flags.learning_rate, trainable=False)
+      # self.lr_update = tf.assign(self.learning_rate, self.learning_rate * flags.learning_rate_decay_factor)
+
+      # pre train
+      pre_losses = self.get_pre_losses()
+      self.pre_loss = tf.add_n(pre_losses, '%s_pre_loss' % dis_scope)
+      pre_optimizer = utils.get_opt(flags, self.learning_rate, opt_epsilon=flags.dis_opt_epsilon)
+      self.pre_update = pre_optimizer.minimize(self.pre_loss, global_step=self.global_step)
+
+      # gan train
+      gan_losses = []
+      gan_losses.append(tf.losses.sigmoid_cross_entropy(self.dis_label_ph, sample_logits))
+      # gan_losses.extend(self.get_regularization_losses())
+      self.gan_loss = tf.add_n(gan_losses, name='%s_gan_loss' % dis_scope)
+      # gan_optimizer = tf.train.GradientDescentOptimizer(self.learning_rate)
+      gan_optimizer = utils.get_opt(flags, self.learning_rate, opt_epsilon=flags.dis_opt_epsilon)
+      self.gan_update = gan_optimizer.minimize(self.gan_loss, global_step=self.global_step)
+
+  def get_regularization_losses(self):
+    regularization_losses = []
+    for regularization_loss in tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES):
+      if not regularization_loss.name.startswith(self.dis_scope):
+        continue
+      regularization_losses.append(regularization_loss)
+    return regularization_losses
+
+  def get_pre_losses(self):
+    pre_losses = [tf.losses.softmax_cross_entropy(self.hard_label_ph, self.logits)]
+    print('#pre_losses=%d' % (len(pre_losses)))
+    pre_losses.extend(self.get_regularization_losses())
+    print('#pre_losses=%d' % (len(pre_losses)))
+    return pre_losses
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ kdgan/mdlcompr_kd/trials/mlp/pretrain_gen.py
@@ -0,0 +1,101 @@
+from kdgan import config
+from kdgan import metric
+from kdgan import utils
+from gen_model import GEN
+import data_utils
+
+from os import path
+from tensorflow.contrib import slim
+from tensorflow.examples.tutorials.mnist import input_data
+import time
+import numpy as np
+import tensorflow as tf
+
+# dataset
+tf.app.flags.DEFINE_string('dataset_dir', None, '')
+tf.app.flags.DEFINE_integer('channels', 1, '')
+tf.app.flags.DEFINE_integer('image_size', 28, '')
+tf.app.flags.DEFINE_integer('num_label', 10, '')
+# model
+tf.app.flags.DEFINE_float('gen_keep_prob', 0.88, '')
+tf.app.flags.DEFINE_string('gen_checkpoint_dir', None, '')
+tf.app.flags.DEFINE_string('gen_save_path', None, '')
+# optimization
+tf.app.flags.DEFINE_float('gen_weight_decay', 0.00004, 'l2 coefficient')
+tf.app.flags.DEFINE_float('gen_opt_epsilon', 1e-6, '')
+tf.app.flags.DEFINE_float('clip_norm', 10.0, '')
+tf.app.flags.DEFINE_float('adam_beta1', 0.9, '')
+tf.app.flags.DEFINE_float('adam_beta2', 0.999, '')
+tf.app.flags.DEFINE_float('rmsprop_momentum', 0.0, '')
+tf.app.flags.DEFINE_float('rmsprop_decay', 0.9, '')
+tf.app.flags.DEFINE_integer('batch_size', 128, '')
+tf.app.flags.DEFINE_integer('num_epoch', 200, '')
+tf.app.flags.DEFINE_string('optimizer', 'rmsprop', 'adam|sgd')
+# learning rate
+tf.app.flags.DEFINE_float('gen_learning_rate', 0.01, '')
+tf.app.flags.DEFINE_float('gen_learning_rate_decay_factor', 0.94, '')
+tf.app.flags.DEFINE_float('gen_num_epochs_per_decay', 2.0, '')
+tf.app.flags.DEFINE_float('end_learning_rate', 0.0001, '')
+tf.app.flags.DEFINE_string('learning_rate_decay_type', 'exponential', 'fixed|polynomial')
+flags = tf.app.flags.FLAGS
+
+mnist = data_utils.read_data_sets(flags.dataset_dir,
+    one_hot=True,
+    validation_size=0,
+    reshape=True)
+print('tn size=%d vd size=%d' % (mnist.train.num_examples, mnist.test.num_examples))
+tn_num_batch = int(flags.num_epoch * mnist.train.num_examples / flags.batch_size)
+vd_num_batch = int(mnist.train.num_examples / config.valid_batch_size)
+print('tn #batch=%d vd #batch=%d' % (tn_num_batch, vd_num_batch))
+eval_interval = int(mnist.train.num_examples / flags.batch_size)
+print('ev #interval=%d' % (eval_interval))
+
+tn_gen = GEN(flags, mnist.train, is_training=True)
+scope = tf.get_variable_scope()
+scope.reuse_variables()
+vd_gen = GEN(flags, mnist.test, is_training=False)
+
+tf.summary.scalar(tn_gen.learning_rate.name, tn_gen.learning_rate)
+tf.summary.scalar(tn_gen.pre_loss.name, tn_gen.pre_loss)
+summary_op = tf.summary.merge_all()
+init_op = tf.global_variables_initializer()
+
+def main(_):
+  utils.delete_if_exist(flags.gen_checkpoint_dir)
+  gen_ckpt = tf.train.latest_checkpoint(flags.gen_checkpoint_dir)
+  # print('gen ckpt=%s' % (gen_ckpt))
+  if gen_ckpt != None:
+    print('todo init from gen ckpt')
+    exit()
+  utils.create_if_nonexist(flags.gen_checkpoint_dir)
+
+  best_acc_v = -np.inf
+  start = time.time()
+  with tf.train.MonitoredTrainingSession() as sess:
+    sess.run(init_op)
+    writer = tf.summary.FileWriter(config.logs_dir, graph=tf.get_default_graph())
+    for tn_batch in range(tn_num_batch):
+      tn_image_np, tn_label_np = mnist.train.next_batch(flags.batch_size)
+      feed_dict = {tn_gen.image_ph:tn_image_np, tn_gen.hard_label_ph:tn_label_np}
+      _, summary = sess.run([tn_gen.pre_update, summary_op], feed_dict=feed_dict)
+      writer.add_summary(summary, tn_batch)
+
+      if (tn_batch + 1) % eval_interval != 0:
+        continue
+      vd_image_np, vd_label_np = mnist.test.images, mnist.test.labels
+      feed_dict = {vd_gen.image_ph:vd_image_np}
+      predictions, = sess.run([vd_gen.predictions], feed_dict=feed_dict)
+      acc_v = metric.compute_acc(predictions, vd_label_np)
+      tot_time = time.time() - start
+      # print('#%08d hit=%.4f %06ds' % (tn_batch, acc_v, int(tot_time)))
+
+      if acc_v < best_acc_v:
+        continue
+      best_acc_v = acc_v
+      global_step, = sess.run([tn_gen.global_step])
+      print('#%08d curacc=%.4f %.0fs' % (global_step, best_acc_v, tot_time))
+      tn_gen.saver.save(utils.get_session(sess), flags.gen_save_path, global_step=global_step)
+  print('bstacc=%.4f' % (best_acc_v))
+
+if __name__ == '__main__':
+  tf.app.run()
