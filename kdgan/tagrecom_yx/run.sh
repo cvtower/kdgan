1d1c343aff37c619fb2ae1b75a79016b97fbd175
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-02-09 11:33:34 +1100

    tag recommendation two way

--- kdgan/tagrecom_yx/run.sh
+++ kdgan/tagrecom_yx/run.sh
@@ -3,7 +3,7 @@ checkpoint_dir=$kdgan_dir/checkpoints
 pretrained_dir=$checkpoint_dir/pretrained
 figure_data_dir=$kdgan_dir/figure_data
 
-#"""
+
 python train_kdgan.py \
   --dataset=yfcc10k \
   --model_name=vgg_16 \
@@ -23,8 +23,8 @@ python train_kdgan.py \
   --num_tch_epoch=10
 # 12517s best hit=0.7973
 exit
-#"""
 
+"""
 
 python pretrain_tch.py \
   --dataset=yfcc10k \
@@ -61,25 +61,6 @@ python pretrain_gen.py \
 # 386s best hit=0.7707
 exit
 """
-python train_kdgan.py \
-  --dataset=yfcc10k \
-  --model_name=vgg_16 \
-  --dis_model_ckpt=$checkpoint_dir/dis_vgg_16.ckpt \
-  --gen_model_ckpt=$checkpoint_dir/gen_vgg_16.ckpt \
-  --tch_model_ckpt=$checkpoint_dir/tch.ckpt \
-  --kdgan_figure_data=$figure_data_dir/kdgan_vgg_16.csv \
-  --feature_size=4096 \
-  --kd_lamda=0.9999 \
-  --temperature=10.0 \
-  --dis_weight_decay=0.0 \
-  --gen_weight_decay=0.0 \
-  --tch_weight_decay=0.0 \
-  --num_epoch=200 \
-  --num_dis_epoch=20 \
-  --num_gen_epoch=10 \
-  --num_tch_epoch=10
-# 12517s best hit=0.7973
-exit
 
 python train_gan.py \
   --dataset=yfcc10k \
@@ -91,7 +72,7 @@ python train_gan.py \
   --dis_weight_decay=0.0 \
   --gen_weight_decay=0.0 \
   --learning_rate=0.05 \
-  --num_epoch=200 \
+  --num_epoch=500 \
   --num_dis_epoch=20 \
   --num_gen_epoch=10
 # best hit=0.7817
