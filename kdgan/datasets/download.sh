610aee2174b3b12f888f8a21bebdf2e0545ced01
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-02-09 16:43:22 +1100

    x

--- kdgan/datasets/download.sh
+++ kdgan/datasets/download.sh
@@ -1,5 +1,5 @@
 server=xiaojie@10.100.228.181 # xw
-server=xiaojie@10.100.228.149 # cz
+# server=xiaojie@10.100.228.149 # cz
 
 ################################################################
 #
@@ -29,13 +29,10 @@ echo $dst_data_dir
 src_precomputed_dir=$src_data_dir/Precomputed
 dst_precomputed_dir=$dst_data_dir/Precomputed
 [ -d ${dst_precomputed_dir} ] || mkdir -p ${dst_precomputed_dir}
-scp ${src_precomputed_dir}/*.npy ${dst_precomputed_dir}
-exit
-
 
 model_name=vgg_16
 scp ${src_precomputed_dir}/${dataset}_${model_name}_000.valid.tfrecord ${dst_precomputed_dir}
-for i in $(seq -w 000 499)
+for i in $(seq -w 000 49)
 do
   filename=${dataset}_${model_name}_${i}.train.tfrecord
   if [ -f ${dst_precomputed_dir}/$filename ]
--- kdgan/tagrecom_xw/run.sh
+++ kdgan/tagrecom_xw/run.sh
@@ -4,6 +4,18 @@ pretrained_dir=$checkpoint_dir/pretrained
 datafig_dir=$kdgan_dir/datafigs
 
 
+python pretrain_gen.py \
+  --dataset=yfcc10k \
+  --model_name=vgg_16 \
+  --gen_model_ckpt=$checkpoint_dir/gen_vgg_16.ckpt \
+  --gen_figure_data=$figure_data_dir/gen_vgg_16.csv \
+  --feature_size=4096 \
+  --learning_rate=0.05 \
+  --num_epoch=200
+# 386s best hit=0.7707
+exit
+
+
 python pretrain_tch.py \
   --dataset=yfcc10k \
   --model_name=vgg_16 \
@@ -98,16 +110,6 @@ python train_gan.py \
 # best hit=0.7817
 exit
 
-python pretrain_gen.py \
-  --dataset=yfcc10k \
-  --model_name=vgg_16 \
-  --gen_model_ckpt=$checkpoint_dir/gen_vgg_16.ckpt \
-  --gen_figure_data=$figure_data_dir/gen_vgg_16.csv \
-  --feature_size=4096 \
-  --learning_rate=0.05 \
-  --num_epoch=200
-# 386s best hit=0.7707
-exit
 
 python train_kd.py \
   --dataset=yfcc10k \
