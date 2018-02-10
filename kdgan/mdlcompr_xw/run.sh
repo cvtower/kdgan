217d4a931515f6de769a902007133322325f3336
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-02-10 17:37:43 +1100

    x

--- kdgan/mdlcompr_xw/run.sh
+++ kdgan/mdlcompr_xw/run.sh
@@ -2,16 +2,30 @@ kdgan_dir=$HOME/Projects/kdgan_xw/kdgan
 checkpoint_dir=$kdgan_dir/checkpoints
 
 train_size=50
+batch_size=5
+
+python train_kd.py \
+  --gen_ckpt_file=$checkpoint_dir/mdlcompr_mnist${train_size}_gen \
+  --tch_ckpt_file=$checkpoint_dir/mdlcompr_mnist${train_size}_tch \
+  --dataset_dir=$HOME/Projects/data/mnist \
+  --gen_model_name=mlp \
+  --tch_model_name=lenet \
+  --optimizer=adam \
+  --train_size=$train_size \
+  --batch_size=$batch_size
+  --num_epoch=200 \
+  --kd_hard_pct=1.0 \
+  --temperature=1.0 \
+exit
+
 
 python pretrain_dis.py \
   --dis_ckpt_file=$checkpoint_dir/mdlcompr_mnist${train_size}_dis \
   --dataset_dir=$HOME/Projects/data/mnist \
   --dis_model_name=lenet \
   --optimizer=adam \
-  --dis_learning_rate=0.001 \
-  --learning_rate_decay_type=fixed \
   --train_size=$train_size \
-  --batch_size=5 \
+  --batch_size=$batch_size \
   --num_epoch=200
 #mnist=50 bstacc=0.6536
 exit
@@ -22,10 +36,8 @@ python pretrain_tch.py \
   --dataset_dir=$HOME/Projects/data/mnist \
   --tch_model_name=lenet \
   --optimizer=adam \
-  --tch_learning_rate=0.001 \
-  --learning_rate_decay_type=fixed \
   --train_size=$train_size \
-  --batch_size=5 \
+  --batch_size=$batch_size \
   --num_epoch=200
 #mnist=50 bstacc=0.6507
 exit
@@ -36,11 +48,10 @@ python pretrain_gen.py \
   --dataset_dir=$HOME/Projects/data/mnist \
   --gen_model_name=mlp \
   --optimizer=adam \
-  --gen_learning_rate=0.001 \
-  --learning_rate_decay_type=fixed \
   --train_size=$train_size \
-  --batch_size=5 \
+  --batch_size=$batch_size \
   --num_epoch=200
+#mnist=50 bstacc=0.5376
 exit
 
 
@@ -83,24 +94,6 @@ python train_gan.py \
   --num_gen_epoch=10
 # bstacc=0.7839 # 500 train examples
 # bstacc=0.9548 # 5000 train examples
-exit
-
-
-python train_kd.py \
-  --kd_hard_pct=1.0 \
-  --temperature=1.0 \
-  --gen_checkpoint_dir=$checkpoint_dir/mdlcompr_mnist_gen \
-  --tch_checkpoint_dir=$checkpoint_dir/mdlcompr_mnist_tch \
-  --dataset_dir=$HOME/Projects/data/mnist \
-  --tch_model_name=lenet_v1 \
-  --tch_keep_prob=0.75 \
-  --optimizer=adam \
-  --tch_opt_epsilon=1e-8 \
-  --gen_learning_rate=0.001 \
-  --gen_learning_rate_decay_factor=0.98 \
-  --learning_rate_decay_type=exponential \
-  --train_size=$train_size \
-  --num_epoch=200
 exit
 
 
