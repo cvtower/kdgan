0746c4dba5f455168ec5bfd4595585e1119ac4b5
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-02-10 17:44:03 +1100

    xg

--- kdgan/mdlcompr_xw/train_kd.py
+++ kdgan/mdlcompr_xw/train_kd.py
@@ -3,56 +3,16 @@ from kdgan import metric
 from kdgan import utils
 from gen_model import GEN
 from tch_model import TCH
+from flags import flags
 import data_utils
 
 from os import path
 from tensorflow.contrib import slim
-from tensorflow.examples.tutorials.mnist import input_data
 import math
 import os
 import time
 import numpy as np
 import tensorflow as tf
-
-# dataset
-tf.app.flags.DEFINE_string('dataset_dir', None, '')
-tf.app.flags.DEFINE_integer('channels', 1, '')
-tf.app.flags.DEFINE_integer('image_size', 28, '')
-tf.app.flags.DEFINE_integer('num_label', 10, '')
-tf.app.flags.DEFINE_integer('train_size', 60000, '')
-tf.app.flags.DEFINE_integer('valid_size', 0, '')
-# model
-tf.app.flags.DEFINE_float('gen_keep_prob', 0.95, '')
-tf.app.flags.DEFINE_float('tch_keep_prob', 0.5, '')
-tf.app.flags.DEFINE_float('kd_hard_pct', 0.3, '')
-tf.app.flags.DEFINE_float('temperature', 3.0, '')
-tf.app.flags.DEFINE_string('gen_checkpoint_dir', None, '')
-tf.app.flags.DEFINE_string('tch_checkpoint_dir', None, '')
-tf.app.flags.DEFINE_string('tch_model_name', None, '')
-tf.app.flags.DEFINE_string('preprocessing_name', None, '')
-# optimization
-tf.app.flags.DEFINE_float('gen_weight_decay', 0.00004, 'l2 coefficient')
-tf.app.flags.DEFINE_float('gen_opt_epsilon', 1e-6, '')
-tf.app.flags.DEFINE_float('tch_weight_decay', 0.00004, 'l2 coefficient')
-tf.app.flags.DEFINE_float('tch_opt_epsilon', 1e-6, '')
-tf.app.flags.DEFINE_float('clip_norm', 10.0, '')
-tf.app.flags.DEFINE_float('adam_beta1', 0.9, '')
-tf.app.flags.DEFINE_float('adam_beta2', 0.999, '')
-tf.app.flags.DEFINE_float('rmsprop_momentum', 0.0, '')
-tf.app.flags.DEFINE_float('rmsprop_decay', 0.9, '')
-tf.app.flags.DEFINE_integer('batch_size', 128, '')
-tf.app.flags.DEFINE_integer('num_epoch', 200, '')
-tf.app.flags.DEFINE_string('optimizer', 'adam', 'rmsprop|sgd')
-# learning rate
-tf.app.flags.DEFINE_float('gen_learning_rate', 0.01, '')
-tf.app.flags.DEFINE_float('gen_learning_rate_decay_factor', 0.94, '')
-tf.app.flags.DEFINE_float('gen_num_epochs_per_decay', 2.0, '')
-tf.app.flags.DEFINE_float('tch_learning_rate', 0.01, '')
-tf.app.flags.DEFINE_float('tch_learning_rate_decay_factor', 0.94, '')
-tf.app.flags.DEFINE_float('tch_num_epochs_per_decay', 2.0, '')
-tf.app.flags.DEFINE_float('end_learning_rate', 0.0001, '')
-tf.app.flags.DEFINE_string('learning_rate_decay_type', 'exponential', 'fixed|polynomial')
-flags = tf.app.flags.FLAGS
 
 mnist = data_utils.read_data_sets(flags.dataset_dir,
     one_hot=True,
@@ -87,21 +47,18 @@ for variable in tf.trainable_variables():
 print('%-50s (%d params)' % ('kd', tot_params))
 
 def main(_):
-  gen_model_ckpt = utils.get_latest_ckpt(flags.gen_checkpoint_dir)
-  tch_model_ckpt = utils.get_latest_ckpt(flags.tch_checkpoint_dir)
-
-  bst_gen_acc = 0.0
-  start = time.time()
+  writer = tf.summary.FileWriter(config.logs_dir, graph=tf.get_default_graph())
+  bst_acc = 0.0
   with tf.train.MonitoredTrainingSession() as sess:
-    writer = tf.summary.FileWriter(config.logs_dir, graph=tf.get_default_graph())
     sess.run(init_op)
-    tn_gen.saver.restore(sess, gen_model_ckpt)
-    tn_tch.saver.restore(sess, tch_model_ckpt)
-    ini_gen_acc = metric.eval_mdlcompr(sess, vd_gen, mnist)
-    ini_tch_acc = metric.eval_mdlcompr(sess, vd_tch, mnist)
-    tot_time = time.time() - start
-    print('inigen=%.4f initch=%.4f tot=%.0fs' % (ini_gen_acc, ini_tch_acc, tot_time))
+    tn_gen.saver.restore(sess, gen_ckpt_file)
+    tn_tch.saver.restore(sess, tch_ckpt_file)
+    ini_gen = metric.eval_mdlcompr(sess, vd_gen, mnist)
+    ini_tch = metric.eval_mdlcompr(sess, vd_tch, mnist)
+    print('inigen=%.4f initch=%.4f' % (ini_gen, ini_tch))
+    exit()
 
+    start = time.time()
     for tn_batch in range(tn_num_batch):
       tn_image_np, tn_label_np = mnist.train.next_batch(flags.batch_size)
 
