90f6d07114757938b4a1314e59b8e613eb5c70e3
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-02-15 09:19:45 +1100

    x

--- kdgan/tagrecom_xw/run_eval.sh
+++ kdgan/tagrecom_xw/run_eval.sh
@@ -7,7 +7,7 @@ train_dataset=yfcc9k
 test_dataset=yfcc0k
 
 python eval_model.py \
-  --gen_model_ckpt=$proj_dir/kdgan_ys/kdgan/checkpoints/kdgan_allit \
+  --gen_checkpoint_dir=$proj_dir/kdgan_ys/kdgan/checkpoints/kdgan_allit \
   --gen_model_run=${runs_dir}/${train_dataset}_${test_dataset}_kdgan.run \
   --dataset=yfcc10k \
   --image_model=vgg_16 \
