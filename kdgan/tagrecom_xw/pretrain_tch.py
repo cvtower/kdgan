8762e1fc065d1d93821de86f5b569135dc27f14d
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-02-21 17:47:31 +1100

    x

--- kdgan/tagrecom_xw/pretrain_tch.py
+++ kdgan/tagrecom_xw/pretrain_tch.py
@@ -13,49 +13,46 @@ import tensorflow as tf
 from os import path
 from tensorflow.contrib import slim
 
-train_data_size = utils.get_tn_size(flags.dataset)
-valid_data_size = utils.get_vd_size(flags.dataset)
-num_batch_t = int(flags.num_epoch * train_data_size / flags.batch_size)
-num_batch_v = int(valid_data_size / config.valid_batch_size)
-eval_interval = int(train_data_size / flags.batch_size)
-print('tn:\t#batch=%d\nvd:\t#batch=%d\neval:\t#interval=%d' % (
-    num_batch_t, num_batch_v, eval_interval))
+tn_size = utils.get_tn_size(flags.dataset)
+vd_size = utils.get_vd_size(flags.dataset)
+print('#tn_data=%d #vd_data=%d' % (tn_size, vd_size))
+tn_num_batch = int(flags.num_epoch * tn_size / flags.batch_size)
+print('#batch=%d' % (tn_num_batch))
+eval_interval = int(tn_size / flags.batch_size)
+
+tch_t = TCH(flags, is_training=True)
+scope = tf.get_variable_scope()
+scope.reuse_variables()
+tch_v = TCH(flags, is_training=False)
+
+tf.summary.scalar(tch_t.learning_rate.name, tch_t.learning_rate)
+tf.summary.scalar(tch_t.pre_loss.name, tch_t.pre_loss)
+summary_op = tf.summary.merge_all()
+init_op = tf.global_variables_initializer()
+
+for variable in tf.trainable_variables():
+  num_params = 1
+  for dim in variable.shape:
+    num_params *= dim.value
+  print('%-50s (%d params)' % (variable.name, num_params))
+
+data_sources_t = utils.get_data_sources(flags, is_training=True, single_source=True)
+print('#tfrecord=%d' % (len(data_sources_t)))
+
+ts_list_t = utils.decode_tfrecord(flags, data_sources_t, shuffle=True)
+bt_list_t = utils.generate_batch(ts_list_t, flags.batch_size)
+user_bt_t, image_bt_t, text_bt_t, label_bt_t, file_bt_t = bt_list_t
+
+image_np, text_np, label_np_v, imgid_np = utils.get_valid_data(flags)
 
 def main(_):
-  tch_t = TCH(flags, is_training=True)
-  scope = tf.get_variable_scope()
-  scope.reuse_variables()
-  tch_v = TCH(flags, is_training=False)
-
-  tf.summary.scalar(tch_t.learning_rate.name, tch_t.learning_rate)
-  tf.summary.scalar(tch_t.pre_loss.name, tch_t.pre_loss)
-  summary_op = tf.summary.merge_all()
-  init_op = tf.global_variables_initializer()
-
-  for variable in tf.trainable_variables():
-    num_params = 1
-    for dim in variable.shape:
-      num_params *= dim.value
-    print('%-50s (%d params)' % (variable.name, num_params))
-
-  data_sources_t = utils.get_data_sources(flags, is_training=True, single_source=True)
-  data_sources_v = utils.get_data_sources(flags, is_training=False)
-  print('tn: #tfrecord=%d\nvd: #tfrecord=%d' % (len(data_sources_t), len(data_sources_v)))
-
-  ts_list_t = utils.decode_tfrecord(flags, data_sources_t, shuffle=True)
-  ts_list_v = utils.decode_tfrecord(flags, data_sources_v, shuffle=False)
-  bt_list_t = utils.generate_batch(ts_list_t, flags.batch_size)
-  bt_list_v = utils.generate_batch(ts_list_v, config.valid_batch_size)
-  user_bt_t, image_bt_t, text_bt_t, label_bt_t, file_bt_t = bt_list_t
-  user_bt_v, image_bt_v, text_bt_v, label_bt_v, file_bt_v = bt_list_v
-
   best_hit_v = -np.inf
   start = time.time()
   with tf.Session() as sess:
     sess.run(init_op)
     writer = tf.summary.FileWriter(config.logs_dir, graph=tf.get_default_graph())
     with slim.queues.QueueRunners(sess):
-      for batch_t in range(num_batch_t):
+      for batch_t in range(tn_num_batch):
         text_np_t, label_np_t = sess.run([text_bt_t, label_bt_t])
         feed_dict = {tch_t.text_ph:text_np_t, tch_t.hard_label_ph:label_np_t}
         _, summary = sess.run([tch_t.pre_update, summary_op], feed_dict=feed_dict)
@@ -63,15 +60,12 @@ def main(_):
 
         if (batch_t + 1) % eval_interval != 0:
             continue
-
-        hit_v = []
-        for batch_v in range(num_batch_v):
-          text_np_v, label_np_v = sess.run([text_bt_v, label_bt_v])
-          feed_dict = {tch_v.text_ph:text_np_v}
-          logit_np_v, = sess.run([tch_v.logits], feed_dict=feed_dict)
-          hit_bt = metric.compute_hit(logit_np_v, label_np_v, flags.cutoff)
-          hit_v.append(hit_bt)
-        hit_v = np.mean(hit_v)
+        feed_dict = {
+          tch_v.image_ph:image_np,
+          tch_v.text_ph:text_np,
+        }
+        logit_np_v = sess.run(tch_v.logits, feed_dict=feed_dict)
+        hit_v = metric.compute_hit(logit_np_v, label_np_v, flags.cutoff)
 
         tot_time = time.time() - start
         print('#%08d hit=%.4f %06ds' % (batch_t, hit_v, int(tot_time)))
