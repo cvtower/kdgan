fe47014475f63e2c7ceafd079f92e567bf978afc
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-02-22 14:39:29 +1100

    tagrecom text and image

--- kdgan/tagrecom_xw/pretrain_tch.py
+++ kdgan/tagrecom_xw/pretrain_tch.py
@@ -37,7 +37,7 @@ for variable in tf.trainable_variables():
   print('%-50s (%d params)' % (variable.name, num_params))
 
 tn_data_sources = utils.get_data_sources(flags, is_training=True, single_source=False)
-tn_data_sources = utils.get_data_sources(flags, is_training=False, single_source=False)
+# tn_data_sources = utils.get_data_sources(flags, is_training=False, single_source=False)
 print('#tfrecord=%d' % (len(tn_data_sources)))
 
 tn_ts_list = utils.decode_tfrecord(flags, tn_data_sources, shuffle=True)
