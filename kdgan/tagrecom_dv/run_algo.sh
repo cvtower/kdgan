6391ad0df1bcb1d16163ae5051cb130c6b6a472a
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-02-24 16:01:33 +1100

    x

---
+++ kdgan/tagrecom_dv/run_algo.sh
@@ -0,0 +1,13 @@
+kdgan_dir=$HOME/Projects/kdgan_xw/kdgan
+checkpoint_dir=$kdgan_dir/checkpoints
+pretrained_dir=$checkpoint_dir/pretrained
+
+python pretrain_gen.py \
+  --dataset=yfcc10k \
+  --model_name=vgg_16 \
+  --gen_model_ckpt=$checkpoint_dir/gen_vgg_16.ckpt \
+  --feature_size=4096 \
+  --learning_rate=0.05 \
+  --num_epoch=200
+exit
+
