cd544245c505fbdfec622a515a2b22df475dba59
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-02-24 16:18:01 +1100

    x

--- kdgan/tagrecom_dv/run_algo.sh
+++ kdgan/tagrecom_dv/run_algo.sh
@@ -5,10 +5,24 @@ pretrained_dir=$checkpoint_dir/pretrained
 gen_model_ckpt=${checkpoint_dir}/gen_vgg_16.ckpt
 dis_model_ckpt=${checkpoint_dir}/dis_vgg_16.ckpt
 
-python pretrain_dis.py \
+python train_gan.py \
+  --dis_model_ckpt=${dis_model_ckpt} \
+  --gen_model_ckpt=${gen_model_ckpt} \
   --dataset=yfcc10k \
   --model_name=vgg_16 \
   --image_model=vgg_16 \
+  --feature_size=4096 \
+  --dis_weight_decay=0.0 \
+  --gen_weight_decay=0.0 \
+  --learning_rate=0.05 \
+  --num_epoch=500 \
+  --num_dis_epoch=20 \
+  --num_gen_epoch=10
+exit
+
+python pretrain_dis.py \
+  --dataset=yfcc10k \
+  --model_name=vgg_16 \
   --dis_model_ckpt=${dis_model_ckpt} \
   --feature_size=4096 \
   --learning_rate=0.05 \
