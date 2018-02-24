f0ec74d99d4a2b88701adba4237af7530c793bae
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-02-24 14:33:05 +1100

    x

--- kdgan/tagrecom_xw/pretrain_gen.py
+++ kdgan/tagrecom_xw/pretrain_gen.py
@@ -58,7 +58,7 @@ def main(_):
       prec = yfcceval.compute_prec(flags, sess, vd_gen)
       best_prec = max(prec, best_prec)
       tot_time = time.time() - start
-      global_step = sess.run(tn_tch.global_step)
+      global_step = sess.run(tn_gen.global_step)
       avg_time = (tot_time / global_step) * (tn_size / flags.batch_size)
       print('#%08d prec@%d=%.4f best=%.4f tot=%.0fs avg=%.2fs/epoch' % 
           (tn_batch, prec, flags.cutoff, best_prec, tot_time, avg_time))
