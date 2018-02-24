63184e928ef7094ef5a693cc2886578f4c6f30b8
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-02-24 20:35:33 +1100

    x

--- kdgan/tagrecom_fv/run_algo.sh
+++ kdgan/tagrecom_fv/run_algo.sh
@@ -29,6 +29,20 @@ python pretrain_dis.py \
   --num_epoch=200
 exit
 
+python train_gan.py \
+  --dis_model_ckpt=${dis_model_ckpt} \
+  --gen_model_ckpt=${gen_model_ckpt} \
+  --dataset=$dataset \
+  --image_model=${image_model} \
+  --optimizer=sgd \
+  --learning_rate_decay_type=fix \
+  --dis_learning_rate=0.05 \
+  --gen_learning_rate=0.05 \
+  --num_epoch=200 \
+  --num_dis_epoch=20 \
+  --num_gen_epoch=10
+exit
+
 python pretrain_gen.py \
   --gen_model_ckpt=${gen_model_ckpt} \
   --dataset=$dataset \
@@ -37,22 +51,6 @@ python pretrain_gen.py \
   --learning_rate_decay_type=fix \
   --gen_learning_rate=0.1 \
   --num_epoch=200
-exit
-
-python train_gan.py \
-  --dataset=yfcc10k \
-  --model_name=vgg_16 \
-  --image_model=vgg_16 \
-  --dis_model_ckpt=$checkpoint_dir/dis_vgg_16.ckpt \
-  --gen_model_ckpt=$checkpoint_dir/gen_vgg_16.ckpt \
-  --gan_figure_data=$figure_data_dir/gan_vgg_16.csv \
-  --feature_size=4096 \
-  --dis_weight_decay=0.0 \
-  --gen_weight_decay=0.0 \
-  --learning_rate=0.05 \
-  --num_epoch=200 \
-  --num_dis_epoch=20 \
-  --num_gen_epoch=10
 exit
 
 python train_kdgan.py \
