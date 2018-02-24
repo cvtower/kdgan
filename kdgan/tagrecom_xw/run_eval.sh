f8adedf88ddd5d6aab680d44ad601be6617f0fc4
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-02-24 17:43:13 +1100

    x

--- kdgan/tagrecom_xw/run_eval.sh
+++ kdgan/tagrecom_xw/run_eval.sh
@@ -9,12 +9,23 @@ feature_size=4096
 train_dataset=yfcc9k
 test_dataset=yfcc0k
 
+kdgan_dir=$HOME/Projects/kdgan_xw/kdgan
+checkpoint_dir=${kdgan_dir}/checkpoints
+pretrained_dir=${checkpoint_dir}/pretrained
+
+variant=basic
+dataset=yfcc10k
+dis_model_ckpt=${checkpoint_dir}/dis_$variant.ckpt
+gen_model_ckpt=${checkpoint_dir}/gen_$variant.ckpt
+tch_model_ckpt=${checkpoint_dir}/tch_$variant.ckpt
+
+
 python eval_model.py \
   --dataset=$dataset \
   --image_model=${image_model} \
   --feature_size=${feature_size} \
   --model_name=gen \
-  --model_ckpt=${kdgan_dir}/kdgan/checkpoints/gen_vgg_16.ckpt \
+  --model_ckpt=${gen_model_ckpt} \
   --model_run=${runs_dir}/${train_dataset}_${test_dataset}_gen.run
 exit
 
