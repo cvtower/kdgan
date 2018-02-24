92f2b1d8dcdc85ba1659ca6a825c916f8208d68b
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-02-24 21:11:49 +1100

    x

--- kdgan/tagrecom_fv/run_algo.sh
+++ kdgan/tagrecom_fv/run_algo.sh
@@ -1,13 +1,22 @@
 kdgan_dir=$HOME/Projects/kdgan_xw/kdgan
 checkpoint_dir=$kdgan_dir/checkpoints
 pretrained_dir=$checkpoint_dir/pretrained
-figure_data_dir=$kdgan_dir/figure_data
 
 python pretrain_gen.py \
   --dataset=yfcc10k \
   --model_name=vgg_16 \
   --image_model=vgg_16 \
   --gen_model_ckpt=$checkpoint_dir/gen_vgg_16.ckpt \
+  --feature_size=4096 \
+  --learning_rate=0.05 \
+  --num_epoch=200
+exit
+
+python pretrain_dis.py \
+  --dataset=yfcc10k \
+  --model_name=vgg_16 \
+  --image_model=vgg_16 \
+  --dis_model_ckpt=$checkpoint_dir/dis_vgg_16.ckpt \
   --feature_size=4096 \
   --learning_rate=0.05 \
   --num_epoch=200
@@ -27,16 +36,6 @@ python train_gan.py \
   --num_epoch=200 \
   --num_dis_epoch=20 \
   --num_gen_epoch=10
-exit
-
-python pretrain_dis.py \
-  --dataset=yfcc10k \
-  --model_name=vgg_16 \
-  --image_model=vgg_16 \
-  --dis_model_ckpt=$checkpoint_dir/dis_vgg_16.ckpt \
-  --feature_size=4096 \
-  --learning_rate=0.05 \
-  --num_epoch=200
 exit
 
 python train_kdgan.py \
