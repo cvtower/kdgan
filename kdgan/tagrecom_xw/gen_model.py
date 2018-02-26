eaecf77486c637fe948b174e2cdece6ccbfeb4eb
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-02-26 14:06:35 +1100

    x

--- kdgan/tagrecom_xw/gen_model.py
+++ kdgan/tagrecom_xw/gen_model.py
@@ -105,8 +105,14 @@ class GEN():
     elif flags.kd_model == 'distn':
       gen_logits = self.logits * (1.0 / flags.temperature)
       tch_logits = self.soft_logit_ph * (1.0 / flags.temperature)
-      soft_loss = tf.losses.mean_squared_error(tch_logits, gen_logits)
-      soft_loss *= pow(flags.temperature, 2.0)
+
+      # soft_loss = tf.losses.mean_squared_error(tch_logits, gen_logits)
+      # soft_loss *= pow(flags.temperature, 2.0)
+
+      gen_labels = tf.softmax(gen_logits)
+      tch_labels = tf.softmax(tch_logits)
+      soft_loss = -1.0 * tf.reduce_mean(tf.log(gen_labels) * tch_labels)
+
       soft_loss *= flags.kd_soft_pct
     else:
       raise ValueError('bad kd model %s', flags.kd_model)
