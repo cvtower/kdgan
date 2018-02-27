354453d57b9ad5140951ebbbf0979ceca52022a0
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-02-28 08:19:34 +1100

    x

--- kdgan/pltfigure/plot.py
+++ kdgan/pltfigure/plot.py
@@ -2,6 +2,8 @@ from kdgan import config
 from kdgan import utils
 
 import pickle
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
