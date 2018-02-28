74b12645fc95418ba77f9a624ddec48ef4091577
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-03-01 10:26:49 +1100

    x

--- kdgan/pltfigure/download.sh
+++ kdgan/pltfigure/download.sh
@@ -6,6 +6,12 @@ src_yfcc_dir=/home/xiaojie/${pickle_dir}
 dst_yfcc_dir=$HOME/${pickle_dir}
 [ -d $dst_yfcc_dir ] || mkdir -p $dst_yfcc_dir
 
+################################################################
+#
+# convergence rate
+#
+################################################################
+
 num_epoch=200
 # scp ${xw_server}:${src_yfcc_dir}/tagrecom_yfcc10k_gen@${num_epoch}.p ${dst_yfcc_dir}
 # scp ${xw_server}:${src_yfcc_dir}/tagrecom_yfcc10k_tch@${num_epoch}.p ${dst_yfcc_dir}
@@ -13,7 +19,25 @@ num_epoch=200
 # scp ${xw_server}:${src_yfcc_dir}/tagrecom_yfcc10k_kdgan@${num_epoch}.p ${dst_yfcc_dir}
 
 train_size=50
-scp ${xw_server}:${src_yfcc_dir}/mdlcompr_mnist${train_size}_gen@${num_epoch}.p ${dst_yfcc_dir}
-scp ${xw_server}:${src_yfcc_dir}/mdlcompr_mnist${train_size}_tch@${num_epoch}.p ${dst_yfcc_dir}
-scp ${cz_server}:${src_yfcc_dir}/mdlcompr_mnist${train_size}_gan@${num_epoch}.p ${dst_yfcc_dir}
-scp ${xw_server}:${src_yfcc_dir}/mdlcompr_mnist${train_size}_kdgan@${num_epoch}.p ${dst_yfcc_dir}
+# scp ${xw_server}:${src_yfcc_dir}/mdlcompr_mnist${train_size}_gen@${num_epoch}.p ${dst_yfcc_dir}
+# scp ${xw_server}:${src_yfcc_dir}/mdlcompr_mnist${train_size}_tch@${num_epoch}.p ${dst_yfcc_dir}
+# scp ${cz_server}:${src_yfcc_dir}/mdlcompr_mnist${train_size}_gan@${num_epoch}.p ${dst_yfcc_dir}
+# scp ${xw_server}:${src_yfcc_dir}/mdlcompr_mnist${train_size}_kdgan@${num_epoch}.p ${dst_yfcc_dir}
+
+################################################################
+#
+# parameter tuning
+#
+################################################################
+
+scp ${cz_server}:${src_yfcc_dir}/mdlcompr_mnist*_kdgan_*.p ${dst_yfcc_dir}
+scp ${xw_server}:${src_yfcc_dir}/mdlcompr_mnist*_kdgan_*.p ${dst_yfcc_dir}
+
+
+
+
+
+
+
+
+
