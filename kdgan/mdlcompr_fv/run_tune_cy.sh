2a7822553d3e3fbab2a9500022826beaa9d20595
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-03-04 11:57:31 +1100

    x

--- kdgan/mdlcompr_fv/run_tune_cy.sh
+++ kdgan/mdlcompr_fv/run_tune_cy.sh
@@ -23,35 +23,34 @@ tune() {
 
         epk_learning_curve_p=${pickle_dir}/mdlcompr_mnist${train_size}_kdgan_${alpha}_${beta}_${gamma}.p
         echo ${epk_learning_curve_p}
-        # python train_kdgan.py \
-        #   --dis_model_ckpt=$checkpoint_dir/mdlcompr_mnist${train_size}_dis \
-        #   --gen_model_ckpt=$checkpoint_dir/mdlcompr_mnist${train_size}_gen \
-        #   --tch_model_ckpt=$checkpoint_dir/mdlcompr_mnist${train_size}_tch \
-        #   --epk_learning_curve_p=${epk_learning_curve_p} \
-        #   --dataset_dir=$HOME/Projects/data/mnist \
-        #   --dis_model_name=lenet \
-        #   --gen_model_name=mlp \
-        #   --tch_model_name=lenet \
-        #   --optimizer=adam \
-        #   --train_size=$train_size \
-        #   --batch_size=$batch_size \
-        #   --dis_learning_rate=1e-3 \
-        #   --gen_learning_rate=5e-4 \
-        #   --tch_learning_rate=5e-4 \
-        #   --num_epoch=100 \
-        #   --num_dis_epoch=20 \
-        #   --num_gen_epoch=10 \
-        #   --num_tch_epoch=10 \
-        #   --kdgan_model=odgan \
-        #   --num_negative=20 \
-        #   --num_positive=5 \
-        #   --kd_model=mimic \
-        #   --noisy_ratio=0.1 \
-        #   --noisy_sigma=0.1 \
-        #   --intelltch_weight=${intelltch_weight} \
-        #   --distilled_weight=${distilled_weight}
-
-
+        python train_kdgan.py \
+          --dis_model_ckpt=$checkpoint_dir/mdlcompr_mnist${train_size}_dis \
+          --gen_model_ckpt=$checkpoint_dir/mdlcompr_mnist${train_size}_gen \
+          --tch_model_ckpt=$checkpoint_dir/mdlcompr_mnist${train_size}_tch \
+          --epk_learning_curve_p=${epk_learning_curve_p} \
+          --dataset_dir=$HOME/Projects/data/mnist \
+          --dis_model_name=lenet \
+          --gen_model_name=mlp \
+          --tch_model_name=lenet \
+          --optimizer=adam \
+          --train_size=$train_size \
+          --batch_size=$batch_size \
+          --dis_learning_rate=1e-3 \
+          --gen_learning_rate=5e-4 \
+          --tch_learning_rate=5e-4 \
+          --num_epoch=100 \
+          --num_dis_epoch=20 \
+          --num_gen_epoch=10 \
+          --num_tch_epoch=10 \
+          --kdgan_model=odgan \
+          --num_negative=20 \
+          --num_positive=5 \
+          --kd_model=mimic \
+          --noisy_ratio=0.1 \
+          --noisy_sigma=0.1 \
+          --alpha=$alpha \
+          --beta=$beta \
+          --gamma=$gamma
       done
     done
   done
