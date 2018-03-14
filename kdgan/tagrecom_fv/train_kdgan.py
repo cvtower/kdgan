81bea54335dd08069002081631524c17c756b59a
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-03-14 16:56:01 +1100

    x

--- kdgan/tagrecom_fv/train_kdgan.py
+++ kdgan/tagrecom_fv/train_kdgan.py
@@ -16,8 +16,7 @@ import tensorflow as tf
 from os import path
 from tensorflow.contrib import slim
 
-print('alpha=%.8f beta=%.8f gamma=%.8f' % (flags.intelltch_weight, flags.distilled_weight, flags.intellstd_weight))
-exit()
+# print('alpha=%.8f beta=%.8f gamma=%.8f' % (flags.intelltch_weight, flags.distilled_weight, flags.intellstd_weight))
 
 tn_size = utils.get_tn_size(flags.dataset)
 eval_interval = int(tn_size / flags.batch_size)
