7fbef80984118ea1118615c6af80affd6dc21b58
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-03-21 16:31:08 +1100

    mdlcompr cifar start

---
+++ kdgan/mdlcompr_cifar/predict.py
@@ -0,0 +1,54 @@
+import numpy as np
+import tensorflow as tf
+from sklearn.metrics import confusion_matrix
+
+from include.data import get_data_set
+from include.model import model
+
+# test_x, test_y, test_l = get_data_set("test", cifar=10)
+train_x, train_y, train_l, mu, std = get_data_set(cifar=10, whitten=False)
+test_x, test_y, test_l, mu, std = get_data_set(name="test", mu=mu, std=std, cifar=10, whitten=False)
+x, y, output, global_step, y_pred_cls, keep_prob = model()
+
+_IMG_SIZE = 32
+_NUM_CHANNELS = 3
+_BATCH_SIZE = 128
+_CLASS_SIZE = 10
+_SAVE_PATH = "./tensorboard/aug-decay-RMS/"
+
+saver = tf.train.Saver()
+sess = tf.Session()
+
+try:
+    print("Trying to restore last checkpoint ...")
+    last_chk_path = tf.train.latest_checkpoint(checkpoint_dir=_SAVE_PATH)
+    saver.restore(sess, save_path=last_chk_path)
+    print("Restored checkpoint from:", last_chk_path)
+except:
+    print("Failed to restore checkpoint. Initializing variables instead.")
+    sess.run(tf.global_variables_initializer())
+
+
+i = 0
+predicted_class = np.zeros(shape=len(test_x), dtype=np.int)
+while i < len(test_x):
+    j = min(i + _BATCH_SIZE, len(test_x))
+    batch_xs = test_x[i:j, :]
+    batch_ys = test_y[i:j, :]
+    predicted_class[i:j] = sess.run(y_pred_cls, feed_dict={x: batch_xs, y: batch_ys, keep_prob: 1.0})
+    i = j
+
+correct = (np.argmax(test_y, axis=1) == predicted_class)
+acc = correct.mean() * 100
+correct_numbers = correct.sum()
+print("Accuracy on Test-Set: {0:.2f}% ({1} / {2})".format(acc, correct_numbers, len(test_x)))
+
+cm = confusion_matrix(y_true=np.argmax(test_y, axis=1), y_pred=predicted_class)
+for i in range(_CLASS_SIZE):
+    class_name = "({}) {}".format(i, test_l[i])
+    print(cm[i, :], class_name)
+class_numbers = [" ({0})".format(i) for i in range(_CLASS_SIZE)]
+print("".join(class_numbers))
+
+
+sess.close()
