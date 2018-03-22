7cd86bd711fde01f5712e9c8d80688d08d12f6f3
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-03-23 10:22:50 +1100

    x

--- kdgan/mdlcompr_cifar/chk_model.py
+++ kdgan/mdlcompr_cifar/chk_model.py
@@ -2,6 +2,7 @@ from kdgan import config
 
 import keras
 import tensorflow as tf
+from keras import backend as K
 from keras.models import Sequential
 from keras.layers import Conv2D, Dense, Flatten, InputLayer, MaxPooling2D
 from keras.objectives import categorical_crossentropy
@@ -9,33 +10,40 @@ from keras.regularizers import l2
 
 weight_decay  = 0.0001
 
-image_ph = tf.placeholder(tf.float32, shape=(None, 32, 32, 3))
-hard_label_ph = tf.placeholder(tf.float32, shape=(None, 10))
+with tf.Session() as sess:
+  K.set_session(sess)
 
-model = Sequential()
-model.add(InputLayer(input_tensor=image_ph, input_shape=(None, 32, 32, 3)))
-model.add(Conv2D(6, (5, 5), padding='valid', activation = 'relu', kernel_initializer='he_normal', input_shape=(32,32,3)))
-model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-model.add(Conv2D(16, (5, 5), padding='valid', activation = 'relu', kernel_initializer='he_normal'))
-model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-model.add(Flatten())
-# model.add(Dense(120, activation = 'relu', kernel_initializer='he_normal'))
-# model.add(Dense(84, activation = 'relu', kernel_initializer='he_normal'))
-# model.add(Dense(10, activation = 'softmax', kernel_initializer='he_normal'))
-model.add(Dense(120, activation = 'relu', kernel_initializer='he_normal', kernel_regularizer=l2(weight_decay) ))
-model.add(Dense(84, activation = 'relu', kernel_initializer='he_normal', kernel_regularizer=l2(weight_decay) ))
-model.add(Dense(10, activation = 'softmax', kernel_initializer='he_normal', kernel_regularizer=l2(weight_decay) ))
+  image_ph = tf.placeholder(tf.float32, shape=(None, 32, 32, 3))
+  hard_label_ph = tf.placeholder(tf.float32, shape=(None, 10))
 
-labels = model.output
-print('label', type(labels), labels.shape)
-losses = model.losses
-for loss in losses:
-  print('loss', type(loss), loss.shape)
+  model = Sequential()
+  model.add(InputLayer(input_tensor=image_ph, input_shape=(None, 32, 32, 3)))
+  model.add(Conv2D(6, (5, 5), padding='valid', activation = 'relu', kernel_initializer='he_normal', input_shape=(32,32,3)))
+  model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+  model.add(Conv2D(16, (5, 5), padding='valid', activation = 'relu', kernel_initializer='he_normal'))
+  model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+  model.add(Flatten())
+  # model.add(Dense(120, activation = 'relu', kernel_initializer='he_normal'))
+  # model.add(Dense(84, activation = 'relu', kernel_initializer='he_normal'))
+  # model.add(Dense(10, activation = 'softmax', kernel_initializer='he_normal'))
+  model.add(Dense(120, activation = 'relu', kernel_initializer='he_normal', kernel_regularizer=l2(weight_decay) ))
+  model.add(Dense(84, activation = 'relu', kernel_initializer='he_normal', kernel_regularizer=l2(weight_decay) ))
+  model.add(Dense(10, activation = 'softmax', kernel_initializer='he_normal', kernel_regularizer=l2(weight_decay) ))
 
-pre_loss = tf.reduce_mean(categorical_crossentropy(labels, hard_label_ph))
-print('pre_loss', type(pre_loss), pre_loss.shape)
+  labels = model.output
+  print('label', type(labels), labels.shape)
+  reg_losses = model.losses
+  for loss in losses:
+    print('loss', type(loss), loss.shape)
 
+  hard_loss = tf.reduce_mean(categorical_crossentropy(labels, hard_label_ph))
+  print('hard_loss', type(hard_loss), hard_loss.shape)
 
+  pre_losses = [hard_loss]
+  pre_losses.extend(reg_losses)
 
+  pre_loss = tf.add_n(pre_losses)
+  pre_update = tf.train.GradientDescentOptimizer(0.1).minimize(pre_loss)
 
+  
 
