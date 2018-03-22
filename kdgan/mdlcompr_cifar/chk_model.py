0138885447cee75b7418f37fb407c15083ccc431
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-03-23 09:58:42 +1100

    x

--- kdgan/mdlcompr_cifar/chk_model.py
+++ kdgan/mdlcompr_cifar/chk_model.py
@@ -3,8 +3,10 @@ from kdgan import config
 import keras
 import tensorflow as tf
 from keras.models import Sequential
-from keras.layers import Conv2D, Dense, Flatten, MaxPooling2D
-from keras.layers import InputLayer
+from keras.layers import Conv2D, Dense, Flatten, InputLayer, MaxPooling2D
+from keras.regularizers import l2
+
+weight_decay  = 0.0001
 
 image_ph = tf.placeholder(tf.float32, shape=(None, 32, 32, 3))
 
@@ -15,9 +17,12 @@ model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 model.add(Conv2D(16, (5, 5), padding='valid', activation = 'relu', kernel_initializer='he_normal'))
 model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 model.add(Flatten())
-model.add(Dense(120, activation = 'relu', kernel_initializer='he_normal'))
-model.add(Dense(84, activation = 'relu', kernel_initializer='he_normal'))
-model.add(Dense(10, activation = 'softmax', kernel_initializer='he_normal'))
+# model.add(Dense(120, activation = 'relu', kernel_initializer='he_normal'))
+# model.add(Dense(84, activation = 'relu', kernel_initializer='he_normal'))
+# model.add(Dense(10, activation = 'softmax', kernel_initializer='he_normal'))
+model.add(Dense(120, activation = 'relu', kernel_initializer='he_normal', kernel_regularizer=l2(weight_decay) ))
+model.add(Dense(84, activation = 'relu', kernel_initializer='he_normal', kernel_regularizer=l2(weight_decay) ))
+model.add(Dense(10, activation = 'softmax', kernel_initializer='he_normal', kernel_regularizer=l2(weight_decay) ))
 
 labels = model.output
 print(type(labels))
