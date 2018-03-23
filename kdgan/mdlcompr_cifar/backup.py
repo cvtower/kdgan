7c9bd14c9dcd96fd431a170267f10ee33be21921
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-03-23 13:51:01 +1100

    x

---
+++ kdgan/mdlcompr_cifar/backup.py
@@ -0,0 +1,85 @@
+from kdgan import config
+import data_utils
+
+import keras
+import tensorflow as tf
+from keras import backend as K
+from keras.models import Sequential
+from keras.layers import Conv2D, Dense, Flatten, InputLayer, MaxPooling2D
+from keras.metrics import categorical_accuracy
+from keras.objectives import categorical_crossentropy
+from keras.regularizers import l2
+
+num_epoch = 200
+batch_size = 128
+weight_decay  = 0.0001
+
+cifar = data_utils.CIFAR()
+tn_data_size = cifar.train.num_examples
+tn_num_batch = int(num_epoch * tn_data_size / batch_size)
+print('train #data=%d #batch=%d' % (tn_data_size, tn_num_batch))
+eval_interval = int(max(tn_data_size / batch_size, 1.0))
+
+import tensorflow as tf
+sess = tf.Session()
+
+from keras import backend as K
+K.set_session(sess)
+
+image_ph = tf.placeholder(tf.float32, shape=(None, 32, 32, 3))
+hard_label_ph = tf.placeholder(tf.float32, shape=(None, 10))
+
+model = Sequential()
+model.add(InputLayer(input_tensor=image_ph, input_shape=(None, 32, 32, 3)))
+model.add(Conv2D(6, (5, 5), padding='valid', activation = 'relu', kernel_initializer='he_normal', input_shape=(32,32,3)))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+model.add(Conv2D(16, (5, 5), padding='valid', activation = 'relu', kernel_initializer='he_normal'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+model.add(Flatten())
+model.add(Dense(120, activation = 'relu', kernel_initializer='he_normal'))
+model.add(Dense(84, activation = 'relu', kernel_initializer='he_normal'))
+model.add(Dense(10, activation = 'softmax', kernel_initializer='he_normal'))
+
+labels = model.output
+print('label', type(labels), labels.shape)
+accuracy = tf.reduce_mean(categorical_accuracy(hard_label_ph, labels))
+
+reg_losses = model.losses
+for reg_loss in reg_losses:
+  print('reg_loss', type(reg_loss), reg_loss.shape)
+
+hard_loss = tf.reduce_mean(categorical_crossentropy(labels, hard_label_ph))
+print('hard_loss', type(hard_loss), hard_loss.shape)
+
+pre_losses = [hard_loss]
+pre_losses.extend(reg_losses)
+
+pre_loss = tf.add_n(pre_losses)
+pre_update = tf.train.GradientDescentOptimizer(0.1).minimize(pre_loss)
+
+
+
+init_op = tf.global_variables_initializer()
+sess.run(init_op)
+
+with sess.as_default():
+  for tn_batch in range(tn_num_batch):
+    tn_image_np, tn_label_np = cifar.train.next_batch(batch_size)
+    feed_dict = {
+      image_ph:tn_image_np,
+      hard_label_ph:tn_label_np,
+      K.learning_phase(): 1,
+    }
+    # res = sess.run(pre_update, feed_dict=feed_dict)
+    pre_update.run(feed_dict=feed_dict)
+
+    if ((tn_batch + 1) % eval_interval != 0) and (tn_batch != (tn_num_batch - 1)):
+      continue
+    feed_dict = {
+      image_ph:cifar.valid.images,
+      hard_label_ph:cifar.valid.labels,
+      K.learning_phase(): 0,
+    }
+    acc = sess.run(accuracy, feed_dict=feed_dict)
+    acc = accuracy.eval(feed_dict=feed_dict)
+    print('#batch=%d acc=%.4f' % (tn_batch, acc))
--- kdgan/mdlcompr_cifar/chk_model.py
+++ kdgan/mdlcompr_cifar/chk_model.py
@@ -1,87 +1,76 @@
-from kdgan import config
-import data_utils
-
-import keras
 import tensorflow as tf
+sess = tf.Session()
+
 from keras import backend as K
+K.set_session(sess)
+
+# this placeholder will contain our input digits, as flat vectors
+img = tf.placeholder(tf.float32, shape=(None, 784))
+
+from keras.layers import Dense
+# Keras layers can be called on TensorFlow tensors:
+# x = Dense(128, activation='relu')(img)  # fully-connected layer with 128 units and ReLU activation
+# x = Dense(128, activation='relu')(x)
+# preds = Dense(10, activation='softmax')(x)  # output layer with 10 units and a softmax activation
+
+from keras.layers import Dropout
+# x = Dense(128, activation='relu')(img)
+# x = Dropout(0.5)(x)
+# x = Dense(128, activation='relu')(x)
+# x = Dropout(0.5)(x)
+# preds = Dense(10, activation='softmax')(x)
+
+from keras.layers import InputLayer
 from keras.models import Sequential
-from keras.layers import Conv2D, Dense, Flatten, InputLayer, MaxPooling2D
-from keras.metrics import categorical_accuracy
+# this is our modified Keras model
+model = Sequential()
+model.add(InputLayer(input_tensor=img, input_shape=(None, 784)))
+# build the rest of the model as before
+model.add(Dense(128, activation='relu'))
+model.add(Dense(128, activation='relu'))
+model.add(Dense(10, activation='softmax'))
+preds = model.output
+
+
+labels = tf.placeholder(tf.float32, shape=(None, 10))
+
 from keras.objectives import categorical_crossentropy
-from keras.regularizers import l2
+loss = tf.reduce_mean(categorical_crossentropy(labels, preds))
 
-num_epoch = 200
-batch_size = 128
-weight_decay  = 0.0001
+from tensorflow.examples.tutorials.mnist import input_data
+data_dir = '/home/xiaojie/Projects/data/mnist/'
+mnist_data = input_data.read_data_sets(data_dir, one_hot=True)
 
-cifar = data_utils.CIFAR()
-tn_data_size = cifar.train.num_examples
-tn_num_batch = int(num_epoch * tn_data_size / batch_size)
-print('train #data=%d #batch=%d' % (tn_data_size, tn_num_batch))
-eval_interval = int(max(tn_data_size / batch_size, 1.0))
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
 
+from keras.metrics import categorical_accuracy as accuracy
+acc_value = tf.reduce_mean(accuracy(labels, preds))
 
-with tf.Session() as sess:
-  K.set_session(sess)
+# Initialize all variables
+init_op = tf.global_variables_initializer()
+sess.run(init_op)
 
-  image_ph = tf.placeholder(tf.float32, shape=(None, 32, 32, 3))
-  hard_label_ph = tf.placeholder(tf.float32, shape=(None, 10))
-
-  model = Sequential()
-  model.add(InputLayer(input_tensor=image_ph, input_shape=(None, 32, 32, 3)))
-  model.add(Conv2D(6, (5, 5), padding='valid', activation = 'relu', kernel_initializer='he_normal', input_shape=(32,32,3)))
-  model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-  model.add(Conv2D(16, (5, 5), padding='valid', activation = 'relu', kernel_initializer='he_normal'))
-  model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-  model.add(Flatten())
-  model.add(Dense(120, activation = 'relu', kernel_initializer='he_normal'))
-  model.add(Dense(84, activation = 'relu', kernel_initializer='he_normal'))
-  model.add(Dense(10, activation = 'softmax', kernel_initializer='he_normal'))
-
-  labels = model.output
-  print('label', type(labels), labels.shape)
-  accuracy = tf.reduce_mean(categorical_accuracy(hard_label_ph, labels))
-
-  reg_losses = model.losses
-  for reg_loss in reg_losses:
-    print('reg_loss', type(reg_loss), reg_loss.shape)
-
-  hard_loss = tf.reduce_mean(categorical_crossentropy(labels, hard_label_ph))
-  print('hard_loss', type(hard_loss), hard_loss.shape)
-
-  pre_losses = [hard_loss]
-  pre_losses.extend(reg_losses)
-
-  pre_loss = tf.add_n(pre_losses)
-  pre_update = tf.train.GradientDescentOptimizer(0.1).minimize(pre_loss)
-
-  init_op = tf.global_variables_initializer()
-  sess.run(init_op)
-  for tn_batch in range(tn_num_batch):
-    tn_image_np, tn_label_np = cifar.train.next_batch(batch_size)
+# Run training loop
+with sess.as_default():
+  for i in range(10000):
+    batch = mnist_data.train.next_batch(50)
     feed_dict = {
-      image_ph:tn_image_np,
-      hard_label_ph:tn_label_np,
+      img: batch[0], 
+      labels: batch[1],
       K.learning_phase(): 1,
     }
-    # res = sess.run(pre_update, feed_dict=feed_dict)
-    pre_update.run(feed_dict=feed_dict)
+    train_step.run(feed_dict=feed_dict)
 
-    if ((tn_batch + 1) % eval_interval != 0) and (tn_batch != (tn_num_batch - 1)):
+    if (i % 100) != 0:
       continue
-
     feed_dict = {
-      image_ph:tn_image_np,
-      hard_label_ph:tn_label_np,
+      img: mnist_data.test.images,
+      labels: mnist_data.test.labels,
+      K.learning_phase(): 0,
     }
-    # acc = sess.run(accuracy, feed_dict=feed_dict)
-    acc = accuracy.eval(feed_dict=feed_dict)
-    print('#batch=%d acc=%.4f' % (tn_batch, acc))
+    acc = acc_value.eval(feed_dict=feed_dict)
+    print('#%d acc=%.4f' % (i, acc))
 
-    # feed_dict = {
-    #   image_ph:cifar.valid.images,
-    #   hard_label_ph:cifar.valid.labels,
-    #   K.learning_phase(): 0,
-    # }
-    # acc = sess.run(accuracy, feed_dict=feed_dict)
-    # print('#batch=%d acc=%.4f' % (tn_batch, acc))
+
+
+
