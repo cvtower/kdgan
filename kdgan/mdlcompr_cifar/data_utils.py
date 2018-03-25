1f3b0e73a1605f31618932a7e12dc2e80f0507f6
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-03-26 09:42:14 +1100

    x

--- kdgan/mdlcompr_cifar/data_utils.py
+++ kdgan/mdlcompr_cifar/data_utils.py
@@ -15,11 +15,18 @@ class CIFAR(object):
     tn_image_np, tn_label_np = sess.run([self.tn_image_ts, self.tn_label_ts])
     return tn_image_np, tn_label_np
 
-  def evaluate(self, sess, image_ph, hard_label_ph, accuracy):
+  def evaluate(self, sess, image_ph, hard_label_ph, accuracy, set_phase=False):
     acc_list = []
     for vd_batch in range(self.vd_num_batch):
       vd_image_np, vd_label_np = sess.run([self.vd_image_ts, self.vd_label_ts])
-      feed_dict = {image_ph:vd_image_np, hard_label_ph:vd_label_np}
+      if set_phase:
+        feed_dict = {
+          image_ph:vd_image_np,
+          hard_label_ph:vd_label_np,
+          K.learning_phase(): 0,
+        }
+      else:
+        feed_dict = {image_ph:vd_image_np, hard_label_ph:vd_label_np}
       acc = sess.run(accuracy, feed_dict=feed_dict)
       acc_list.append(acc)
     acc = sum(acc_list) / len(acc_list)
--- kdgan/mdlcompr_cifar/pretrain_tch.py
+++ kdgan/mdlcompr_cifar/pretrain_tch.py
@@ -164,7 +164,7 @@ def main(argv=None):
         sess.run(pre_train, feed_dict=feed_dict)
         if (tn_batch + 1) % eval_interval != 0 and (tn_batch + 1) != tn_num_batch:
           continue
-        acc = cifar.evaluate(sess, image_ph, hard_label_ph, accuracy)
+        acc = cifar.evaluate(sess, image_ph, hard_label_ph, accuracy, set_phase=True)
         bst_acc = max(acc, bst_acc)
 
         end_time = time.time()
