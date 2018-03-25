029e33ef600e33f8230a0282cb60258b37e810b7
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-03-25 15:21:30 +1100

    x

--- kdgan/mdlcompr_cifar/run_algo.sh
+++ kdgan/mdlcompr_cifar/run_algo.sh
@@ -1,15 +1,13 @@
 kdgan_dir=$HOME/Projects/kdgan_xw/kdgan
-checkpoint_dir=$kdgan_dir/checkpoints
-train_size=50
-batch_size=5
+checkpoint_dir=${kdgan_dir}/checkpoints
+train_size=50000
+batch_size=128
 
 python pretrain_std.py \
   --std_model_ckpt=${checkpoint_dir}/mdlcompr_cifar${train_size}_std \
   --dataset_dir=$HOME/Projects/data/cifar \
-  --std_model_name=lenet \
-  --optimizer=adam \
   --train_size=${train_size} \
   --batch_size=${batch_size} \
   --num_epoch=200
-#cifar=50 bstacc=0.6536
+#cifar=50000 final=0.6536
 exit
