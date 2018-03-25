aa4177001dd8275a174624b5d96d12c7e132232f
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-03-26 09:25:25 +1100

    x

--- kdgan/mdlcompr_cifar/pretrain_tch.py
+++ kdgan/mdlcompr_cifar/pretrain_tch.py
@@ -7,9 +7,12 @@ import cifar10_utils
 
 from keras import backend as K
 from keras.initializers import Constant, TruncatedNormal
-from keras.layers import Conv2D, Dense, Flatten, InputLayer, MaxPooling2D
+from keras.layers import add
+from keras.layers import Activation, Conv2D, Dense, Flatten, GlobalAveragePooling2D, \
+    Input, InputLayer, MaxPooling2D
+from keras.layers.normalization import BatchNormalization
 from keras.metrics import categorical_accuracy
-from keras.models import Sequential
+from keras.models import Model, Sequential
 from keras.objectives import categorical_crossentropy
 from keras.regularizers import l2
 import numpy as np
@@ -26,6 +29,7 @@ image_shape = (flags.batch_size, flags.image_size, flags.image_size, flags.chann
 image_ph = tf.placeholder(tf.float32, shape=image_shape)
 hard_label_ph = tf.placeholder(tf.int32, shape=(flags.batch_size))
 
+'''
 model = Sequential()
 model.add(InputLayer(input_tensor=image_ph, input_shape=image_shape))
 model.add(Conv2D(64, (5, 5), 
@@ -61,9 +65,69 @@ model.add(Dense(10,
     kernel_regularizer=None,
     bias_initializer=Constant(value=0.0)))
 logits = model.output
+'''
+
+weight_decay = flags.tch_weight_decay
+def residual_network(img_input,classes_num=10,stack_n=5):
+  def residual_block(x,o_filters,increase=False):
+    stride = (1,1)
+    if increase:
+      stride = (2,2)
+    o1 = Activation('relu')(BatchNormalization(momentum=0.9, epsilon=1e-5)(x))
+    conv_1 = Conv2D(o_filters,kernel_size=(3,3),strides=stride,padding='same',
+                    kernel_initializer="he_normal",
+                    kernel_regularizer=l2(weight_decay))(o1)
+    o2  = Activation('relu')(BatchNormalization(momentum=0.9, epsilon=1e-5)(conv_1))
+    conv_2 = Conv2D(o_filters,kernel_size=(3,3),strides=(1,1),padding='same',
+                    kernel_initializer="he_normal",
+                    kernel_regularizer=l2(weight_decay))(o2)
+    if increase:
+      projection = Conv2D(o_filters,kernel_size=(1,1),strides=(2,2),padding='same',
+                          kernel_initializer="he_normal",
+                          kernel_regularizer=l2(weight_decay))(o1)
+      block = add([conv_2, projection])
+    else:
+      block = add([conv_2, x])
+    return block
+  # build model ( total layers = stack_n * 3 * 2 + 2 )
+  # stack_n = 5 by default, total layers = 32
+  # input: 32x32x3 output: 32x32x16
+  x = Conv2D(filters=16,kernel_size=(3,3),strides=(1,1),padding='same',
+             kernel_initializer="he_normal",
+             kernel_regularizer=l2(weight_decay))(img_input)
+  # input: 32x32x16 output: 32x32x16
+  for _ in range(stack_n):
+    x = residual_block(x,16,False)
+  # input: 32x32x16 output: 16x16x32
+  x = residual_block(x,32,True)
+  for _ in range(1,stack_n):
+    x = residual_block(x,32,False)
+  # input: 16x16x32 output: 8x8x64
+  x = residual_block(x,64,True)
+  for _ in range(1,stack_n):
+    x = residual_block(x,64,False)
+  x = BatchNormalization(momentum=0.9, epsilon=1e-5)(x)
+  x = Activation('relu')(x)
+  x = GlobalAveragePooling2D()(x)
+  # input: 64 output: 10
+  # x = Dense(classes_num,activation='softmax',kernel_initializer="he_normal",
+  #           kernel_regularizer=regularizers.l2(weight_decay))(x)
+  x = Dense(classes_num,activation=None,kernel_initializer="he_normal",
+            kernel_regularizer=l2(weight_decay))(x)
+  return x
+
+img_rows = img_cols = flags.image_size
+img_channels = flags.channels
+num_classes = flags.num_label
+stack_n = 3
+img_input = Input(shape=(img_rows,img_cols,img_channels))
+output    = residual_network(img_input,num_classes,stack_n)
+resnet = Model(img_input, output)
+logits = output
 
 hard_loss = cifar10_utils.loss(logits, hard_label_ph)
-regularization_losses = model.losses
+regularization_losses = resnet.losses
+print('#regularization_losses' % len(regularization_losses))
 pre_losses = [hard_loss]
 pre_losses.extend(regularization_losses)
 pre_loss = tf.add_n(pre_losses)
