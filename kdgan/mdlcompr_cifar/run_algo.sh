ed9cee2f7d776e457efed51544e2492b770a412f
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-03-25 19:02:57 +1100

    x

--- kdgan/mdlcompr_cifar/run_algo.sh
+++ kdgan/mdlcompr_cifar/run_algo.sh
@@ -13,8 +13,7 @@ python pretrain_tch.py \
   --train_size=${train_size} \
   --batch_size=${batch_size} \
   --learning_rate_decay_factor=0.96 \
-  --num_epochs_per_decay=10.0 \
-  --tch_weight_decay=0.0001 \
+  --num_epochs_per_decay=20.0 \
   --num_epoch=200
 #cifar=50000 final=0.6536
 exit
