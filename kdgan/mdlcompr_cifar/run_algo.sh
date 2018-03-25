c9946582c895a88cd637f13bb6f6d90081d2214d
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-03-25 15:27:12 +1100

    x

--- kdgan/mdlcompr_cifar/run_algo.sh
+++ kdgan/mdlcompr_cifar/run_algo.sh
@@ -8,6 +8,8 @@ python pretrain_std.py \
   --dataset_dir=$HOME/Projects/data/cifar \
   --train_size=${train_size} \
   --batch_size=${batch_size} \
-  --num_epoch=20
+  --learning_rate_decay_factor=0.96 \
+  --num_epochs_per_decay=10.0 \
+  --num_epoch=200
 #cifar=50000 final=0.6536
 exit
