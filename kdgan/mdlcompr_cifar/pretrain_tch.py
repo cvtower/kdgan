b7cbf4fa26e6daad9a6710573f5feeab2873ea18
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-03-25 20:33:21 +1100

    s

--- kdgan/mdlcompr_cifar/pretrain_tch.py
+++ kdgan/mdlcompr_cifar/pretrain_tch.py
@@ -26,6 +26,44 @@ image_shape = (flags.batch_size, flags.image_size, flags.image_size, flags.chann
 image_ph = tf.placeholder(tf.float32, shape=image_shape)
 hard_label_ph = tf.placeholder(tf.int32, shape=(flags.batch_size))
 
+from keras.layers.core import Layer
+class LRN(Layer):
+  def __init__(self, n, k=1, alpha=0.0001, beta=0.75, **kwargs):
+    self.n = n
+    self.alpha = alpha
+    self.k = k
+    self.beta = beta
+    super(LRN, self).__init__(**kwargs)
+
+  def call(self, x, mask=None):
+    b, ch, r, c = x.shape
+    half_n = self.n // 2 # half the local region
+    # orig keras code
+    #input_sqr = T.sqr(x)  # square the input
+    input_sqr = K.square(x) # square the input
+    # orig keras code
+    #extra_channels = T.alloc(0., b, ch + 2 * half_n, r,c)  # make an empty tensor with zero pads along channel dimension
+    #input_sqr = T.set_subtensor(extra_channels[:, half_n:half_n+ch, :, :],input_sqr) # set the center to be the squared input
+
+    extra_channels = K.zeros((b, int(ch) + 2 * half_n, r, c))
+    input_sqr = K.concatenate([extra_channels[:, :half_n, :, :],input_sqr, extra_channels[:, half_n + int(ch):, :, :]],axis = 1)
+
+    scale = self.k # offset for the scale
+    norm_alpha = self.alpha / self.n # normalized alpha
+    for i in range(self.n):
+      scale += norm_alpha * input_sqr[:, i:i+int(ch), :, :]
+    scale = scale ** self.beta
+    x = x / scale
+    return x
+
+  def get_config(self):
+    config = {"alpha": self.alpha,
+              "k": self.k,
+              "beta": self.beta,
+              "n": self.n}
+    base_config = super(LRN, self).get_config()
+    return dict(list(base_config.items()) + list(config.items()))
+
 model = Sequential()
 model.add(InputLayer(input_tensor=image_ph, input_shape=image_shape))
 model.add(Conv2D(64, (5, 5), 
@@ -36,6 +74,10 @@ model.add(Conv2D(64, (5, 5),
     bias_initializer=Constant(value=0)))
 model.add(MaxPooling2D((3, 3),
     strides=(2, 2)))
+model.add(LRN(4,
+    k=1.0,
+    alpha=0.001 / 9.0,
+    beta=0.75))
 model.add(Conv2D(64, (5, 5),
     padding='same',
     activation='relu',
