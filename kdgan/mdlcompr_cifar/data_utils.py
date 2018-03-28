69c03d65ba46f44ee2f8ecaa4dfe8c64df28a46f
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-03-28 14:01:24 +1100

    x

--- kdgan/mdlcompr_cifar/data_utils.py
+++ kdgan/mdlcompr_cifar/data_utils.py
@@ -47,9 +47,9 @@ class CIFAR():
       image = tf.random_crop(image, [image_size, image_size, 3])
       image = tf.image.random_flip_left_right(image)
       # brightness/saturation/constrast provides small gains .2%~.5% on cifar
-      image = tf.image.random_brightness(image, max_delta=63. / 255.)
-      image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
-      image = tf.image.random_contrast(image, lower=0.2, upper=1.8)
+      # image = tf.image.random_brightness(image, max_delta=63. / 255.)
+      # image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
+      # image = tf.image.random_contrast(image, lower=0.2, upper=1.8)
       image = tf.image.per_image_standardization(image)
 
       example_queue = tf.RandomShuffleQueue(
