c133493bb1d6f35eda9188f803219f5ad3b435da
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-03-30 13:47:41 +1100

    cifar kd works

--- kdgan/mdlcompr_cifar/train_kd.py
+++ kdgan/mdlcompr_cifar/train_kd.py
@@ -48,10 +48,10 @@ def main(_):
     for tn_batch in range(tn_num_batch):
       tn_image_np, tn_label_np = cifar.next_batch(sess)
 
-      feed_dict = {tn_tch.image_ph:tn_image_np}
-      soft_logit_np = sess.run(tn_tch.logits, feed_dict=feed_dict)
-      # feed_dict = {vd_tch.image_ph:tn_image_np}
-      # soft_logit_np = sess.run(vd_tch.logits, feed_dict=feed_dict)
+      # feed_dict = {tn_tch.image_ph:tn_image_np}
+      # soft_logit_np = sess.run(tn_tch.logits, feed_dict=feed_dict)
+      feed_dict = {vd_tch.image_ph:tn_image_np}
+      soft_logit_np = sess.run(vd_tch.logits, feed_dict=feed_dict)
 
       # predictions = np.argmax(tn_label_np, axis=1)
       # groundtruth = np.argmax(soft_logit_np, axis=1)
