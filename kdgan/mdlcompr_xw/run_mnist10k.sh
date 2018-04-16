a38e40c8c3a8faaf7b09bebc8ae036daa684972a
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-04-16 13:11:59 +1000

    x

--- kdgan/mdlcompr_xw/run_mnist10k.sh
+++ kdgan/mdlcompr_xw/run_mnist10k.sh
@@ -14,9 +14,9 @@ python train_gan.py \
   --optimizer=adam \
   --train_size=${train_size} \
   --batch_size=${batch_size} \
-  --num_epoch=10 \
-  --num_dis_epoch=5 \
-  --num_gen_epoch=5 \
+  --num_epoch=20 \
+  --num_dis_epoch=20 \
+  --num_gen_epoch=10 \
   --num_negative=20 \
   --num_positive=5
 exit
