15866a80d41962715f97f65209ed43bad986b3dc
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-05-10 18:28:42 +1000

    BaGAN

--- kdgan/pltfigure/plt_mdlcompr.py
+++ kdgan/pltfigure/plt_mdlcompr.py
@@ -225,7 +225,7 @@ def conv():
   ax.plot(epoch_np, mimic_prec_np, label='MIMIC', linestyle='--', linewidth=line_width)
   # tch_prec_np = data_utils.get_horizontal_np(epoch_np, 0.6978)
   # ax.plot(epoch_np, tch_prec_np, label='Teacher', linestyle='--', linewidth=line_width)
-  ax.plot(epoch_np, gan_prec_np, label='PGAN', color='r', linewidth=line_width)
+  ax.plot(epoch_np, gan_prec_np, label='BaGAN', color='r', linewidth=line_width)
   ax.plot(epoch_np, kdgan_prec_np, label='KDGAN', color='b', linewidth=line_width)
   ax.set_xlim([0, 100])
   ax.legend(loc='lower right', prop={'size':legend_size})
